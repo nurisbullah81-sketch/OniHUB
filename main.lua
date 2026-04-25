bd5e594d354f59747d98681329a6088a3afeed1f
nurisbullah81-sketch <nurisbullah81@gmail.com>
2026-04-26 03:45:47 +0700

    Update main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+local Settings = {
+    JoinTeam = "Pirates", 
+    AutoHop = true,
+    SafeDelay = 15 -- Jeda biar tidak kena Error 773 (Unauthorized)
+}
+
+-- SCRIPT UTAMA LU DISINI
+print("OniHUB Active!")
+-- (Paste sisa kode DIZX lu di bawah sini)
