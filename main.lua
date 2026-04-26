c9ae1f928b5fd70da3481a564b9e6a1de61b94ed
nurisbullah81-sketch <nurisbullah81@gmail.com>
2026-04-26 19:41:03 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,21 @@
--- CatHUB SUPREMACY v12.0: The Final Restoration
+-- CatHUB SUPREMACY v13.0: Loader
 _G.CatHUB_Loaded = true
 
-local function GetFile(name)
-    local url = "https://raw.githubusercontent.com/nurisbullah81-sketch/OniHUB/refs/heads/main/"..name.."?v="..math.random()
+local function Load(file)
+    local url = "https://raw.githubusercontent.com/nurisbullah81-sketch/OniHUB/refs/heads/main/"..file.."?v="..math.random()
     local success, result = pcall(function() return loadstring(game:HttpGet(url))() end)
     if not success then 
-        warn("[CatHUB]: Critical Error on "..name.." -> "..tostring(result)) 
+        warn("[CatHUB]: Critical Error Loading -> "..file.." | "..tostring(result)) 
     end
     return result
 end
 
--- LOADING ORDER (Pastiin urutan ini bener)
-_G.UI_Lib = GetFile("UI.lua")
-GetFile("ESP.lua")
-GetFile("Fruits.lua")
-GetFile("Combat.lua")
-GetFile("Farm.lua")
-GetFile("Teleport.lua")
+-- INITIALIZATION ORDER
+_G.UI = Load("UI.lua")
+Load("ESP.lua")
+Load("Fruits.lua")
+Load("Combat.lua")
+Load("Farm.lua")
+Load("Teleport.lua")
 
-print("[CatHUB]: v12.0 Absolute Power Deployed.")
+print("[CatHUB]: v13.0 Absolute Restoration Fully Active.")
