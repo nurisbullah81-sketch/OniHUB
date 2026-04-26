c951a0962084734498fe8ebbaca95451214aebd2
nurisbullah81-sketch <nurisbullah81@gmail.com>
2026-04-26 19:38:07 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,16 @@
--- CatHUB SUPREMACY v11.0: THE FINAL ARCHIVE
+-- CatHUB SUPREMACY v12.0: The Final Restoration
 _G.CatHUB_Loaded = true
 
 local function GetFile(name)
     local url = "https://raw.githubusercontent.com/nurisbullah81-sketch/OniHUB/refs/heads/main/"..name.."?v="..math.random()
-    local s, r = pcall(function() return loadstring(game:HttpGet(url))() end)
-    if not s then warn("[CatHUB]: Critical Error on "..name.." -> "..tostring(r)) end
-    return r
+    local success, result = pcall(function() return loadstring(game:HttpGet(url))() end)
+    if not success then 
+        warn("[CatHUB]: Critical Error on "..name.." -> "..tostring(result)) 
+    end
+    return result
 end
 
--- LOADING SEQUENCE
+-- LOADING ORDER (Pastiin urutan ini bener)
 _G.UI_Lib = GetFile("UI.lua")
 GetFile("ESP.lua")
 GetFile("Fruits.lua")
@@ -16,4 +18,4 @@ GetFile("Combat.lua")
 GetFile("Farm.lua")
 GetFile("Teleport.lua")
 
-print("[CatHUB]: v11.0 Reality Override Fully Deployed.")
+print("[CatHUB]: v12.0 Absolute Power Deployed.")
