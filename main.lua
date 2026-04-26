71135ac9c523dc7fce2471f926a46f2da7ef2815
nurisbullah81-sketch <nurisbullah81@gmail.com>
2026-04-27 03:36:59 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,9 @@
-local function Load(f)
-    local url = "https://raw.githubusercontent.com/nurisbullah81-sketch/OniHUB/refs/heads/main/" .. f .. "?v=" .. math.random()
+local function Load(file)
+    local url = "https://raw.githubusercontent.com/nurisbullah81-sketch/OniHUB/refs/heads/main/" .. file .. "?v=" .. math.random()
     local ok, r = pcall(function() return loadstring(game:HttpGet(url))() end)
-    if not ok then warn("[CatHUB] Fail: " .. f) end
+    if not ok then warn("[CatHUB] Fail: " .. file .. " | " .. tostring(r)) end
     return r
 end
-
-_G.Cat = {
-    Player = game:GetService("Players").LocalPlayer,
-    Settings = { FruitESP = false }
-}
 
 Load("StyleUI.lua")
 Load("ESP.lua")
